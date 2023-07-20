cc0e4d32cd0ab92a673d6fe70d398d51b0df6073
BernikovaLera <109979423+BernikovaLera@users.noreply.github.com>
2023-07-20 17:44:32 +0300

    Завершён

---
+++ Projects in C#/Working with MS Office in WPF_3/up_pr10/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Exc = Microsoft.Office.Interop;
+
+namespace up_pr10
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Pi_Click(object sender, RoutedEventArgs e)
+        {
+            var XL = new Microsoft.Office.Interop.Excel.Application();
+            var PI = XL.WorksheetFunction.Pi();
+
+            TextBlock_Pi.Text = Math.PI.ToString();
+
+            XL.Quit();
+        }
+    }
+}
